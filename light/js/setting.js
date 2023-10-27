00ab3857065e8344a78015ff003238f6875e23f7
yunfeit <i@iiiiy.com>
2023-10-28 01:10:54 +0800

    3D音乐律动

---
+++ light/js/setting.js
@@ -0,0 +1,67 @@
+var LIGHTS = LIGHTS || {};
+
+LIGHTS.Config=
+{
+	homeImages:
+	{
+		loadingButton:      'images/home/loadingButton.png',
+		playButton:         'images/home/playButton.png',
+		bokeh:              'images/home/bokeh.png'
+	},
+	musicMP3:               'music.mp3', // Chrome
+	musicOGG:               'music.ogg', // Firefox
+	images:
+	{
+		terrain66:          'images/experience/terrain66.png',
+		plasmaRed:          'images/experience/plasmaRed.png',
+		plasmaYellow:       'images/experience/plasmaYellow.png',
+		plasmaGreen:        'images/experience/plasmaGreen.png',
+		plasmaCyan:         'images/experience/plasmaCyan.png',
+		plasmaBlue:         'images/experience/plasmaBlue.png',
+		plasmaMagenta:      'images/experience/plasmaMagenta.png',
+		plasmaWhite:        'images/experience/plasmaWhite.png',
+		circle:             'images/experience/circle.png',
+		dot:                'images/experience/dot.png',
+		terrainDot:         'images/experience/particle.png',
+		glow:               'images/experience/glow.png',
+		skybox:             'images/experience/skybox.png',
+		bengalSeq:          'images/experience/bengalSeq.png',
+		bengalShadow:       'images/experience/bengalShadow.png',
+		spot:               'images/experience/spot.png',
+		spotLine:           'images/experience/spotLine.png',
+		spotLineAlpha:      'images/experience/spotLineAlpha.png',
+		envMapTop:          'images/envmap/envMapTop.png',
+		envMapBottom:       'images/envmap/envMapBottom.png',
+		envMapFrontBack:    'images/envmap/envMapFrontBack.png',
+		envMapLeftRight:    'images/envmap/envMapLeftRight.png',
+		font:               'fonts/fonts.png',
+		spectrumData:       'images/data/spectrumData.png',
+
+		tweetButton:        'images/home/tweetButton.png',
+		replayButton:       'images/home/replayButton.png',
+	},
+    font:'fonts/fonts.txt',
+	tweetsPre:'',
+	defaultTweets:
+	[
+		'My name is gaoyong',
+		'This page using WebGl and Canvas',
+		'Html5 is cool',
+		'You can use the chrome to browse',
+		'Html5 is support by modern brower',
+		'Canvas is a new tag of html',
+		'You can use canvas draw what you want'
+	],
+	defaultAvatars:
+	[
+		'images/avatars/ellie.jpg',
+		'images/avatars/cherrytree.png',
+		'images/avatars/polydor.png',
+		'images/avatars/interscope.png',
+		'images/avatars/blue.png',
+		'images/avatars/magenta.png',
+		'images/avatars/green.png',
+		'images/avatars/orange.png',
+		'images/avatars/red.png'
+	]
+};
